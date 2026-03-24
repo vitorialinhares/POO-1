c9e71f1a3c427693098ed9eb5e55a8bcbab84f45
mateus <mateuspontesdsilva11@gmail.com>
2026-03-24 15:21:32 -0300

    ex2

---
+++ python/ana.py
@@ -0,0 +1,10 @@
+class P1:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def Apresentar(self):
+        print(self.nome)
+        print(self.idade)
+
+
